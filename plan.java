b49b29bf632e7c8556771877f4b5c8d0f3226be2
Binoja <vv binoja@gmail.com>
2025-04-09 05:30:18 +0530

    this is my second comment

---
+++ plan.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Swan{
+     public static void main(String[] args){
+	 String color= "Black";
+	 switch(color){
+	 
+	 case "Yellow":
+		System.out.println("Yellow");
+	 
+		break;
+	 
+	 case "Green":
+		System.out.println("Green");
+	 
+		break;
+	 
+	 case "Black":
+		System.out.println("Black");
+	 
+		break;
+	 
+	 default:
+	 System.out.println("White");
+	 }
+	 }
+	 }
